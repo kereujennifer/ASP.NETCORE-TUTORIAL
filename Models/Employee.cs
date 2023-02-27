7849c0090202d259a51fc071a42b60962befb79c
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-02-27 12:54:46 +0300

    Select List in ASP.NET Core

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,13 +6,15 @@ namespace EmployeeManagement.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(50, ErrorMessage = "Name too long")]
         public string Name { get; set; }
         [Required]
-        [MaxLength(50, ErrorMessage ="Name too long")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email Format")]
+
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email Format")]
-        public Dept Department { get; set; }
+
+        public Dept? Department { get; set; }
 
     }
 }
