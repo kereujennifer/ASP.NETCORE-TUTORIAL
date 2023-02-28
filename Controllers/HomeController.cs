71e291f7b951f98f4330da90cb471f6ff4ed84ce
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-02-28 17:23:40 +0300

    File upload in asp net core mvc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,24 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System.IO;
+using System;
+using Microsoft.AspNetCore.Hosting;
+
 namespace EmployeeManagement.Controllers
 {
     
     public class HomeController: Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,IHostingEnvironment hostingEnvironment)
         {
 
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
        
         public ViewResult Index()
@@ -37,15 +44,46 @@ namespace EmployeeManagement.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
-                return RedirectToAction("details", new { id = newEmployee.Id })
-            ;
+                string uniqueFileName = null;
+
+                // If the Photo property on the incoming model object is not null, then the user
+                // has selected an image to upload.
+                if (model.Photo != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot
+                    // To get the path of the wwwroot folder we are using the inject
+                    // HostingEnvironment service provided by ASP.NET Core
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    // To make sure the file name is unique we are appending a new
+                    // GUID value and and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    // Use CopyTo() method provided by IFormFile interface to
+                    // copy the file to wwwroot/images folder
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    // Store the file name in PhotoPath property of the employee object
+                    // which gets saved to the Employees database table
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
-           return View();
+
+            return View();
         }
+    
+
     }
 }
---
+++ ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name too long")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email Format")]
+
+        public string Email { get; set; }
+        [Required]
+
+        public Dept? Department { get; set; }
+
+        public IFormFile Photo { get; set; }
+    }
+}
