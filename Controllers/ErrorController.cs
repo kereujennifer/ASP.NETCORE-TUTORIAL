8ff2552d6b824957cfe152fd6680da17552abcec
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-02 17:06:02 +0300

    Logging exceptions in ASP.NET Core

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -1,40 +1,56 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 
 namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
-        [Route("Error/{statusCode}")]
-        public IActionResult HttpsStatusCodeHandler(int statusCode)
+        private readonly ILogger<ErrorController> logger;
+
+        // Inject ASP.NET Core ILogger service. Specify the Controller
+        // Type as the generic parameter. This helps us identify later
+        // which class or controller has logged the exception
+        public ErrorController(ILogger<ErrorController> logger)
         {
-            var statusCodeResult=HttpContext.Features.Get
-<IStatusCodeReExecuteFeature>();        
+            this.logger = logger;
+        }
+
+        [AllowAnonymous]
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            // Retrieve the exception Details
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            // LogError() method logs the exception under Error category in the log
+            logger.LogError($"The path {exceptionHandlerPathFeature.Path} " +
+                $"threw an exception {exceptionHandlerPathFeature.Error}");
+
+            return View("Error");
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult =
+                HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             switch (statusCode)
             {
                 case 404:
-                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
-
+                    ViewBag.ErrorMessage = "Sorry, the resource could not be found";
+                    // LogWarning() method logs the message under
+                    // Warning category in the log
+                    logger.LogWarning($"404 error occured. Path = " +
+                        $"{statusCodeResult.OriginalPath} and QueryString = " +
+                        $"{statusCodeResult.OriginalQueryString}");
                     break;
             }
 
             return View("NotFound");
-        }
-        [Route("Error")]
-        [AllowAnonymous]
-        public IActionResult Error()
-        {
-            var exceptionDetails = HttpContext.Features.Get
-<IExceptionHandlerFeature>();
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
-
-            return View("Error");
-
         }
     }
 }
