23ed67334e8c791e71a2e78f960d46502d592a70
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-03 10:07:31 +0300

    Using NLog in ASP.NET Core

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NLog.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -13,14 +11,19 @@ namespace EmployeeManagement
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                // Remove all the default logging providers
+                logging.ClearProviders();
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                // Add NLog as the Logging Provider
+                logging.AddNLog();
+            })
+           .UseStartup<Startup>();
     }
 }
