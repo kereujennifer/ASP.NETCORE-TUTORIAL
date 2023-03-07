d664b15e7aeb244f948d652b26d0ca4810c4aa1a
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-07 09:00:32 +0300

    Register New Userin ASP.NET Core Identity

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
