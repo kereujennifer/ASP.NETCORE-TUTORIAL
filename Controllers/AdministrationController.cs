852d854f39101eb67900d85b4e70c00d8b04cb64
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-09 13:16:34 +0300

    Creating roles in asp net core

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.Rolename
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                    if(result.Succeeded)
+                    {
+                    return RedirectToAction("index", "home");
+                    }
+                    foreach(IdentityError error in result.Errors)
+                    {
+                    ModelState.AddModelError("", error.Description);
+                    }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string Rolename { get; set; }
+    }
+}
