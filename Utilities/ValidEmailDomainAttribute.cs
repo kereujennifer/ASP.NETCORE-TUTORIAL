2dc2ef5637f4c2bf29a0e290d6553eaca3b002a9
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-09 10:31:23 +0300

    Custom validation attribute in asp net core

---
+++ Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Helpers;
+
+namespace EmployeeManagement.Utilities
+{
+    public class ValidEmailDomainAttribute: ValidationAttribute
+    {
+        private readonly string allowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain)
+        {
+            this.allowedDomain = allowedDomain;
+        }
+        public override bool IsValid(object value)
+        {
+           string[] strings = value.ToString().Split('@');
+            return strings[1].ToUpper() == allowedDomain.ToUpper();
+        }
+    }
+}
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 
@@ -9,6 +10,7 @@ namespace EmployeeManagement.ViewModels
         [Required]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller:"Account")]
+        [ValidEmailDomain(allowedDomain:"kereu.com", ErrorMessage="Email domain name must be kereu.com")]
         public string Email { get; set; }
 
         [Required]
