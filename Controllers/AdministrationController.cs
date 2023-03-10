04367bb98482dee2f08dd12c2d76c95c28e38786
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-10 10:01:53 +0300

    Add or remove users from role in asp net core

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -2,6 +2,9 @@
 using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Graph.Beta.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +15,7 @@ namespace EmployeeManagement.Controllers
         private readonly RoleManager<IdentityRole> roleManager;
         private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager, 
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
                                         UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
@@ -21,12 +24,12 @@ namespace EmployeeManagement.Controllers
         [HttpGet]
         public IActionResult CreateRole()
         {
-            return View();  
+            return View();
         }
         [HttpPost]
         public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 IdentityRole identityRole = new IdentityRole
                 {
@@ -35,14 +38,14 @@ namespace EmployeeManagement.Controllers
 
                 IdentityResult result = await roleManager.CreateAsync(identityRole);
 
-                    if(result.Succeeded)
-                    {
+                if (result.Succeeded)
+                {
                     return RedirectToAction("listroles", "administration");
-                    }
-                    foreach(IdentityError error in result.Errors)
-                    {
+                }
+                foreach (IdentityError error in result.Errors)
+                {
                     ModelState.AddModelError("", error.Description);
-                    }
+                }
             }
             return View(model);
         }
@@ -55,7 +58,7 @@ namespace EmployeeManagement.Controllers
         [HttpGet]
         public async Task<IActionResult> EditRole(string id)
         {
-           var role= await roleManager.FindByIdAsync(id);
+            var role = await roleManager.FindByIdAsync(id);
             if (role == null)
             {
                 ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
@@ -65,11 +68,11 @@ namespace EmployeeManagement.Controllers
             {
                 Id = role.Id,
                 RoleName = role.Name,
-               
+
             };
-            foreach(var user in userManager.Users)
+            foreach (var user in userManager.Users)
             {
-               if(await userManager.IsInRoleAsync(user, role.Name))
+                if (await userManager.IsInRoleAsync(user, role.Name))
                 {
                     model.Users.Add(user.UserName);
                 }
@@ -90,7 +93,7 @@ namespace EmployeeManagement.Controllers
             {
                 role.Name = model.RoleName;
 
-               
+
                 var result = await roleManager.UpdateAsync(role);
 
                 if (result.Succeeded)
@@ -106,6 +109,74 @@ namespace EmployeeManagement.Controllers
                 return View(model);
             }
         }
-    }
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+            var role = await roleManager.FindByIdAsync(roleId);
 
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+            var model = new List<UserRoleViewModel>();
+            foreach (var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                };
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+                model.Add(userRoleViewModel);
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model,string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+            for(int i = 0; i < model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+                IdentityResult result = null;
+                if (model[i].IsSelected && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                  result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].IsSelected && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+
+                }
+                else
+                {
+                    continue;
+                }
+                if (result.Succeeded)
+                {
+                    if (i < (model.Count - 1))
+                        continue;
+                    else
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                }
+            }
+            return RedirectToAction("EditRole", new { Id = roleId });
+        }
+
+    }
 }
