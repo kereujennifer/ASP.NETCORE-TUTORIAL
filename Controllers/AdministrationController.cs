9978e4b0ff8779b46a9d9c3bced130e3f8d19a70
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-10 10:27:15 +0300

    ASP NET Core role based authorization

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Exchange.WebServices.Data;
@@ -10,6 +11,9 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
+    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "User")]
+
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
