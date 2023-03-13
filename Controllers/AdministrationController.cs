734c51b539276fa3efe2918e2a0c234f1ff4c252
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-13 10:21:58 +0300

    Delete identity user in asp net core

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -25,6 +25,33 @@ namespace EmployeeManagement.Controllers
             this.roleManager = roleManager;
             this.userManager = userManager;
         }
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUsers");
+            }
+        }
         [HttpGet]
         public IActionResult ListUsers()
         {
