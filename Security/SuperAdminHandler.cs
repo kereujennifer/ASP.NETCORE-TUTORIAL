d3ee1584123f70e973600761243b95bda36ef60c
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-16 09:41:29 +0300

    Multiple custom authorization handlers for a requirement in asp net core

---
+++ Security/SuperAdminHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Security
+{
+    public class SuperAdminHandler: AuthorizationHandler<ManageAdminRolesAndClaimsRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ManageAdminRolesAndClaimsRequirement requirement)
+        {
+        if(context.User.IsInRole("Super Admin"))
+            {
+                context.Succeed(requirement);
+            }
+        return Task.CompletedTask;  
+        }
+    }
+}
